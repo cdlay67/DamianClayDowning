b3625b5c16f7ba5b1c22af266df084800c1e88f2
Damian C-D <damianclay12@gmail.com>
2018-11-13 18:36:06 -0600

    updating repo

--- index.js
+++
@@ -1,8 +0,0 @@
-var sectionId = anime({
-    targets: '#sectionId .container1',
-    opacity: .5,
-    left: '240px',
-    backgroundColor: '#FFF',
-    borderRadius: ['0em', '2em'],
-    easing: 'easeInOutQuad'
-  });
