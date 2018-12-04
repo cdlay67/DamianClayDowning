6d6b2c6546e6453fa1c1967dbf59c99fb05c6e6d
Damian C-D <damianclay12@gmail.com>
2018-12-03 20:07:00 -0600

    first commit

--- index.js
+++ index.js
@@ -4,13 +4,51 @@ import Content from './src/Content';
 import Footer from './src/Footer';
 import greet from './src/Greeting';
 
-var state = {'title': 'Hello Bloggggggg!!!!'};
+var State = {
+    'active': 'home',
+    
+    'home': {'title': 'Hello Bloggggggg!!!!'},
+    
+    'Blog': {'title': 'Read my blog'},
+    
+    'Contact': {'title': 'any questions or concenrs'},
+    
+    'Projects': {'title': 'what do you want to know'},
 
-document.querySelector('#root').innerHTML = `
-${Navigation()}
-${Header(state)}
-${Content()}
-${Footer()}
+    'Events': {'title': 'what do you want to know'}
+};
+
+var root = document.querySelector('#root');
+
+function handleNavigation(event){
+    event.preventDefault();
+    
+State.active = event.target.textContent;
+
+    render(State);//eslint disable line
+}
+
+
+function render(state){
+var links;
+
+    root.innerHTML = `
+        ${Navigation(state)}
+        ${Header(state)}
+        ${Content(state)}
+        ${Footer(state)}
 `;
 
 greet();
+
+links = document.querySelectorAll('#navigation a');
+
+
+links[0].addEventListener('click', handleNavigation);
+
+links[1].addEventListener('click', handleNavigation);
+
+links[2].addEventListener('click', handleNavigation);
+}
+
+render(State);
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ export default function Header(state) {
     return `
 <div id="header">
 <div class="container">
-    <h1>${state.title}</h1>
+    <h1>${state[state.active].title}</h1>
     <h3>Damian Clay-Downing</h3>
 </div>
 
